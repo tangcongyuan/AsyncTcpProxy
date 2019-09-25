c93e920d24659fb0e8f60f77060d3e7f79776723
Congyuan Tang <cytang@iluvatar.ai>
2019-09-25 15:49:42 -0700

    Change README.md to README for pypi.

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@
 
 from distutils.core import setup
 
-with open('README.md', 'r') as file:
+with open('README', 'r') as file:
     long_description = file.read()
 
 setup(name='AsyncTcpProxy',
-      version='0.4.0',
+      version='0.4.2',
       description='A proxy handles data to/from a TCP server.',
       long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Eric Tang',
       author_email='tangcongyuan@gmail.com',
       url='',
